45d7184399cab823966752c8092df1abc85af607
Daniel-KI <dankorshov@gmail.com>
2021-01-24 18:13:46 +0300

    feat: add solution for extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  //throw new CustomError('Not implemented');
+  let resultStr = '';
+  let onePartOfStr = '';
+  let currentOptions = {
+    repeatTimes: typeof (options.repeatTimes) !== 'undefined' ? options.repeatTimes : 1,
+    separator: typeof (options.separator) !== 'undefined' ? options.separator : '+',
+    addition: typeof (options.addition) !== 'undefined' ? options.addition : '',
+    additionRepeatTimes: typeof (options.additionRepeatTimes) !== 'undefined' ? options.additionRepeatTimes : 0,
+    additionSeparator: typeof (options.additionSeparator) !== 'undefined' ? options.additionSeparator : '|'
+  };
+
+  onePartOfStr = String(str) + currentOptions.addition;
+  for (let i = 0; i < currentOptions.additionRepeatTimes - 1; i++) {
+    onePartOfStr += currentOptions.additionSeparator + currentOptions.addition;
+  }
+
+  resultStr = onePartOfStr;
+  for (let i = 0; i < currentOptions.repeatTimes - 1; i++) {
+    resultStr += currentOptions.separator + onePartOfStr;
+  }
+
+  return resultStr;
 };
-  
