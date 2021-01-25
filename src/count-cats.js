c974b938638d3f260a5d03d4ac2834daad8fa102
Daniel-KI <dankorshov@gmail.com>
2021-01-25 13:05:06 +0300

    refactor: delete comments

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,7 +1,6 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(matrix) {
-  //throw new CustomError('Not implemented');
   let searchStr = '^^';
   let count = matrix.flat().filter(str => str === searchStr);
   return count.length;
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,12 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
-  //throw new CustomError('Not implemented');
   var teamName = '';
   if(Array.isArray(members) === false) return false;
     members.forEach(element => {
       if(typeof(element) === 'string') teamName += element.trim()[0];
     });
-  console.log(teamName);
   return teamName.toUpperCase().split('').sort().join('');
 };
